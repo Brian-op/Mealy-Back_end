b134b46e994abbe0024b46fd435a89a2c6fc1f6e
Brian <brian.kiprono8@student.moringaschool.com>
2025-07-23 18:52:47 +0300

    link controler to authorization URL

---
+++ routes/auth.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request
+from controllers.auth_controller import signup, login
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/signup', methods = ['POST'])
+def handle_signup():
+    return signup(request.json)
+
+@auth_bp.route('/login', methods = ['POST'])
+def handle_login():
+    return login(request.json)
