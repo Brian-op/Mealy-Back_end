b72bbca9716ec02adc7060b5d18539ea518010b3
Brian <brian.kiprono8@student.moringaschool.com>
2025-07-23 17:52:01 +0300

    web token encoding and decoding utility

---
+++ utils/jwt_utils.py
@@ -0,0 +1,19 @@
+import jwt
+from datetime import datetime  
+from flask import current_app
+
+def encoding_token (user_id):
+    payload = {
+    'exp' : datetime.datetime.utcnow()+ datetime.timedelta(hours=24),
+    'iat': datetime.datetime.utcnow(),
+    'sub': user_id
+    }
+    return jwt.encode(payload, current_app.config['SECRET_KEY'], algorithm='HS256')
+
+def decode_token(token):
+    try : 
+        payload =jwt.decode(token,current_app.config['SECRET_KEY'],algorithms=['HS256'])
+        return payload['sub']
+
+    except jwt.ExpiredSignatureError:
+        return None
