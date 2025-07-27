dee29b1665f95b5eb0f038c4fe7352e5e1c67b11
Brian <brian.kiprono8@student.moringaschool.com>
2025-07-27 18:13:28 +0300

    removed routes

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_cors import CORS
-from routes import auth, meals, menus, orders  # Import your route blueprints
-from config import Config  # Your custom config file
+from models import db
+from config import Config
+
+
+from controllers.auth_controller import auth_bp
+from controllers.meal_controller import meal_bp
+from controllers.menu_controller import menu_bp
+from controllers.order_controller import order_bp
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+ 
+    db.init_app(app)
+    Migrate(app, db)
     CORS(app)
 
-    # Register Blueprints
-    app.register_blueprint(auth.auth_bp, url_prefix='/auth')
-   
+
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(meal_bp, url_prefix='/meals')
+    app.register_blueprint(menu_bp, url_prefix='/menus')
+    app.register_blueprint(order_bp, url_prefix='/orders')
 
     return app
+
 
 if __name__ == '__main__':
     app = create_app()
