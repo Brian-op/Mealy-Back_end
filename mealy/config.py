03c320cee3031ea5d9c2a50cdfa2074b46e3beb1
Young Monarch <brian.kiprono8@student.moringaschool.com>
2025-07-30 00:46:53 +0300

    Update config.py

--- mealy/config.py
+++ mealy/config.py
@@ -1,10 +1,12 @@
 import os
 
 class Config:
-  
     SECRET_KEY = os.environ.get("SECRET_KEY", "fallback_secret")
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///mealy.db")
+    db_url = os.environ.get("DATABASE_URL", "sqlite:///mealy.db")
 
- 
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
